18d7c2c82fe9915b3f4f9b54f4ba45c1f35c33a0
rafaelfez <rafafelipesilva@gmail.com>
2016-10-18 23:23:47 -0200

    criando script sql

---
+++ inc/script.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE Sindicato;
+
+USE sindicato;
+
+CREATE TABLE Usuario(
+  login VARCHAR(50) PRIMARY KEY,
+  senha VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Afiliado(
+  matricula INT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  telefone INT NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  nascimento DATETIME NOT NULL,
+  endereco VARCHAR(150),
+  rg INT NOT NULL,
+  cpf INT NOT NULL,
+  celular INT NOT NULL,
+  sexo VARCHAR(10) NOT NULL,
+  situacao VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE Dependente(
+  cpf INT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  telefone INT NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  nascimento DATETIME NOT NULL,
+  endereco VARCHAR(150) NOT NULL,
+  rg INT NOT NULL,
+  celular INT NOT NULL,
+  sexo VARCHAR(10),
+  Afiliado_matricula INT,
+  FOREIGN KEY(Afiliado_matricula) REFERENCES Afiliado(matricula)
+);
+
+CREATE TABLE FolhaDePagamento(
+  idPagamento INT PRIMARY KEY,
+  salario DECIMAL(10,2) NOT NULL,
+  periodo VARCHAR(45) NOT NULL,
+  saldo DECIMAL(10,2) NOT NULL,
+  data DATETIME NOT NULL,
+  adicional DECIMAL(10,2),
+  Afiliado_matricula INT,
+  FOREIGN KEY(Afiliado_matricula) REFERENCES Afiliado(matricula)
+);
+
+CREATE TABLE Convenio(
+  cnpj INT PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL,
+  categoria VARCHAR(45) NOT NULL,
+  empresa VARCHAR(45) NOT NULL,
+  mensalidade VARCHAR(45) NOT NULL,
+  DAS_Afiliado_matricula INT,
+  RCS_Afiliado_matricula INT,
+  FOREIGN KEY(DAS_Afiliado_matricula) REFERENCES DAS(Afiliado_matricula),
+  FOREIGN KEY(RCS_Afiliado_matricula) REFERENCES RCS(Afiliado_matricula)
+);
+
+CREATE TABLE DAS(
+  Afiliado_matricula INT,
+  FOREIGN KEY(Afiliado_matricula) REFERENCES Afiliado(matricula),
+  porcentagem DECIMAL(3,2) NOT NULL,
+  saldo DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE Devolucao_DAS(
+  valor DECIMAL(10,2) NOT NULL,
+  data DATETIME NOT NULL,
+  DAS_Afiliado_matricula INT,
+  FOREIGN KEY(DAS_Afiliado_matricula) REFERENCES DAS(Afiliado_matricula)
+);
+
+CREATE TABLE RCS(
+  Afiliado_matricula INT,
+  FOREIGN KEY(Afiliado_matricula) REFERENCES Afiliado(matricula),
+  porcentagem DECIMAL(3,2),
+  saldo DECIMAL(10,2)
+);
+
+CREATE TABLE Devolucao_RCS(
+  valor DECIMAL(10,2) NOT NULL,
+  data DATETIME NOT NULL,
+  RCS_Afiliado_matricula INT,
+  FOREIGN KEY(RCS_Afiliado_matricula) REFERENCES RCS(Afiliado_matricula)
+);
