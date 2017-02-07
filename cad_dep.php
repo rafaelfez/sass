40f24c8d888701d8af6e93973eff6a5c4ddd7f29
Marcelo Luiz de Acácio <marcelo.acaci@gmail.com>
2017-02-07 01:32:40 -0200

    Validação de campos e implementação de placeholders

--- cad_dep.php
+++ cad_dep.php
@@ -43,55 +43,77 @@ include("inc/header.php");
         }
       ?>
   <h2 class="bg-info">Cadastro de Dependente</h2>
-  <form class="form-group" method="post" action="cad_dep.php">
+  <form class="form-group" data-toggle="validator" method="post" action="cad_dep.php">
     <table>
         <tr>
           <th><label for="afiliado_matricula">Matrícula do Arrumador:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="afiliado_matricula" name="afiliado_matricula" value="<?php echo htmlspecialchars($afiliado_matricula); ?>"/></td>
+          <td><input type="text" required placeholder="" class="form-control" id="afiliado_matricula" name="afiliado_matricula" value="<?php echo htmlspecialchars($afiliado_matricula); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="nome">Nome:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="nome" name="nome" value="<?php echo htmlspecialchars($nome); ?>"/></td>
+          <td><input type="text" required placeholder="" class="form-control" id="nome" name="nome" value="<?php echo htmlspecialchars($nome); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="nascimento">Data de Nascimento:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="nascimento" name="nascimento" value="<?php echo htmlspecialchars($nascimento); ?>"/></td>
+          <td><input type="text" required placeholder="DD/MM/AAAA" class="form-control" id="nascimento" name="nascimento" value="<?php echo htmlspecialchars($nascimento); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="sexo">Sexo:<span class="required">*<span></label></th>
-          <td><select id="sexo" class="form-control" name="sexo">
+          <td><select id="sexo" required class="form-control" name="sexo">
             <option value="">Selecione:</option>
             <option value="Masculino" <?php if($sexo == 'Masculino') echo 'selected'; ?>>Masculino</option>
             <option value="Feminino" <?php if($sexo == 'Feminino') echo 'selected'; ?>>Feminino</option>
-          </select></td>
+          </select>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="telefone">Telefone:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control"v id="telefone" name="telefone" value="<?php echo htmlspecialchars($telefone); ?>"/></td>
+          <td><input type="text" required placeholder="(xx)xxxx-xxxx" class="form-control"v id="telefone" name="telefone" value="<?php echo htmlspecialchars($telefone); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="celular">Celular:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="celular" name="celular" value="<?php echo htmlspecialchars($celular); ?>"/></td>
+          <td><input type="text" required placeholder="(xx)xxxxx-xxxx" class="form-control" id="celular" name="celular" value="<?php echo htmlspecialchars($celular); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="email">Email:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($email); ?>"/></td>
+          <td><input type="text" required placeholder="nome@mail.com" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($email); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="endereco">Endereço:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="endereco" name="endereco" value="<?php echo htmlspecialchars($endereco); ?>"/></td>
+          <td><input type="text" required placeholder="Rua A, número 200, Centro" class="form-control" id="endereco" name="endereco" value="<?php echo htmlspecialchars($endereco); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="rg">RG:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="rg" name="rg" value="<?php echo htmlspecialchars($rg); ?>"/></td>
+          <td><input type="text"required placeholder="Digite somente os números" required placeholder="###" class="form-control" id="rg" name="rg" value="<?php echo htmlspecialchars($rg); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="cpf">CPF:<span class="cpf">*</span></label></th>
-          <td><input type="text" class="form-control" id="cpf" name="cpf" value="<?php echo htmlspecialchars($cpf); ?>"/></td>
+          <td><input type="text" required placeholder="Digite somente os números" class="form-control" id="cpf" name="cpf" value="<?php echo htmlspecialchars($cpf); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
         <tr>
           <th><label for="parentesco">Parentesco:<span class="required">*</span></label></th>
-          <td><input type="text" class="form-control" id="parentesco" name="parentesco" value="<?php echo htmlspecialchars($parentesco); ?>"/></td>
+          <td><input type="text" required placeholder="Ex: Filho, Esposa, etc." class="form-control" id="parentesco" name="parentesco" value="<?php echo htmlspecialchars($parentesco); ?>"/>
+            <div class="help-block with-errors"></div>
+          </td>
         </tr>
     </table>
    <abbr title="Cadastrar Dependente"><input class="btn btn-primary" type="submit" value="Cadastrar"/></abbr>
